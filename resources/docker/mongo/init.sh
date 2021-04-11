6fb83ee3596838ba94c3ef7e25659118df41ed38
Boe Khattiya <keerati.khat@gmail.com>
2021-04-11 20:53:36 +0700

    🌱: tweets

--- resources/docker/mongo/init.sh
+++ resources/docker/mongo/init.sh
@@ -2,6 +2,16 @@
 echo "************************************************************"
 echo "****************        IMPORT DATA        *****************"
 echo "************************************************************"
+DATASETDIR="/data/datasets"
+DATASETDB="/data/db"
+DATASETTWEETS="${DATASETDB}/dataset_tweets.json"
+echo "[]" > ${DATASETTWEETS}
+for TWEETDIR in ${DATASETDIR}/*; do
+  # source tweet
+  for SRCDIR in ${TWEETDIR}/source-tweets/*; do
+    echo $(jq ". += [`cat ${SRCDIR}`]" ${DATASETTWEETS}) > ${DATASETTWEETS}
+  done
+done
 
 # create db
 mongo ${MONGO_INITDB_DATABASE} \
@@ -10,7 +20,9 @@ mongo ${MONGO_INITDB_DATABASE} \
         -p ${MONGO_INITDB_ROOT_PASSWORD} \
         --authenticationDatabase admin \
         --eval "db.createUser({user: '${MONGO_INITDB_ROOT_USERNAME}', pwd: '${MONGO_INITDB_ROOT_PASSWORD}', roles:[{role:'dbOwner', db: '${MONGO_INITDB_DATABASE}'}]});"
-
+# import tweets
+mongoimport -d ${MONGO_INITDB_DATABASE} -c tweets --file ${DATASETTWEETS} --jsonArray
+rm -f ${DATASETTWEETS}
 
 echo "************************************************************"
 echo "************************************************************"
